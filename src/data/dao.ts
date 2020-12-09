fdb4f87e3ac81a6278a68d8888ffd6055544514a
Fabrizio Marconi <Nemolo@users.noreply.github.com>
2020-12-09 09:34:57 +0100

    test retry execute statement for cold starts

--- src/data/dao.ts
+++ src/data/dao.ts
@@ -4,12 +4,23 @@ import { SqlParametersList } from "aws-sdk/clients/rdsdataservice";
 const rdsDataService = new RDSDataService();
 
 export async function executeStatement(sql: string, parameters?: SqlParametersList) {
-  const res = await rdsDataService.executeStatement({
-    resourceArn: process.env.auroraDBArn,
-    secretArn: process.env.secretArn,
-    sql,
-    database: 'guardian',
-    parameters
-  }).promise();
-  return res;
+  try {
+
+    const res = await rdsDataService.executeStatement({
+      resourceArn: process.env.auroraDBArn,
+      secretArn: process.env.secretArn,
+      sql,
+      database: 'guardian',
+      parameters
+    }).promise();
+    return res;
+
+  } catch (e) {
+    if (e.code === 'BadRequestException') {
+      if ((e.message as string).startsWith('Communications link failure')) {
+        return await executeStatement(sql, parameters);
+      }
+    }
+  }
+
 }
