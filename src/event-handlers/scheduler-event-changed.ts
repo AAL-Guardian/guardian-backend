32ccc216ed15542a0a197d1a8985261d4ccafb28
Fabrizio Marconi <fabrizio.marconi@jef.it>
2021-03-30 15:50:35 +0200

    fixed launching scheduler event request

--- src/event-handlers/scheduler-event-changed.ts
+++ src/event-handlers/scheduler-event-changed.ts
@@ -1,4 +1,5 @@
 import { DynamoDBStreamEvent } from "aws-lambda";
+import { checkUserAndLaunchReportRequest } from "../logic/launch-report-request";
 
 export default async function (event: DynamoDBStreamEvent) {
   await Promise.all(event.Records.map(async record => {
@@ -8,8 +9,9 @@ export default async function (event: DynamoDBStreamEvent) {
 
     if (eventName === 'REMOVE') {
       console.log('REMOVE');
-      console.log(dynamodbRecord);
       
+      console.log(dynamodbRecord);
+      await checkUserAndLaunchReportRequest(dynamodbRecord.OldImage.eventId.S);
     } else {
       console.log("Event is " + eventName + ", Skipping execution");
     }
