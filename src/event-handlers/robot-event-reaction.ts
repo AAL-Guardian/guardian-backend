87176971e1aa230bbc046e2d0fb399809c5737ec
Fabrizio Marconi <fabrizio.marconi@jef.it>
2022-06-23 10:56:49 +0200

    minor fix

--- src/event-handlers/robot-event-reaction.ts
+++ src/event-handlers/robot-event-reaction.ts
@@ -2,11 +2,11 @@ import { getRobotByTopic } from '../data/robot';
 import { handleRobotInteraction } from '../logic/guardian-event-logic';
 
 export default async function(event: {
-  topic: string,
+  robot_topic: string,
   data: any
 }) {
   if(event.data?.skill === 'sense_touch') {
-    const robot = await getRobotByTopic(event.topic);
+    const robot = await getRobotByTopic(event.robot_topic);
     if(event.data.message === 'touch detected') {
       await handleRobotInteraction(robot, event.data);
     }
